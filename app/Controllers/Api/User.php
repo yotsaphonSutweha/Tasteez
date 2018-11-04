c37d97c5cbe560a432cc2cb133d65802299bffac
Eoin1539 <eoinkirwan11@gmail.com>
2018-11-04 12:36:32 +0000

    getFavourites and popular api

---
+++ app/Controllers/Api/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tasteez\Controllers\Api;
+use Tasteez\Models\User;
+use Slim\Exception\MethodNotAllowedException;
+
+class Meals extends Controller {
+
+  protected $user;
+  protected $container;
+
+  function __construct($container) {
+    $this->user = new User($container->db);
+    $this->container = $container;
+  }
+
+  public function popular($request, $response) {
+    $meal = new \Tasteez\Models\Meal($this->container->db);
+    return $response->withJson($meal->popular());
+  }
+
+  public function getFavorites($request, $response, $args) {
+    $user = new \Tasteez\Models\User($this->container->db);
+
+    if ($user->isLoggedIn()) {
+        $userDetails = json_decode($_COOKIE['cookie'], true);
+        $userId = $userDetails['id'];
+        return $response->withJson($user->getFavorites($userId));
+    } else {
+        return $response->withJson(array("Message" => "User not logged in!"));
+    }
+  }
+
+}
